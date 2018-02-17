95eb2d1840c737dcc4ecef94d1ec0520a6705579
Michele Locati <michele@locati.it>
2018-02-17 16:46:08 +0100

    Test

---
+++ account/account.ts
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+
+(<any>window).ccm_enableUserProfileMenu = function() {
+    let account = $('#ccm-account-menu');
+    if (account.length) {
+        let container = $('#ccm-account-menu-container');
+        if (container.length === 0) {
+			container = $('<div />').appendTo(document.body);
+		}
+        container.addClass('ccm-ui').attr('id', 'ccm-account-menu-container');
+        $('#ccm-account-menu').appendTo(container);
+
+        let documentHeight = $(document).height();
+        let position = $('#ccm-account-menu').offset().top;
+
+		if (documentHeight > 200 && (documentHeight - position) < 200) {
+			$('#ccm-account-menu').addClass('dropup');
+		}
+    }
+};
